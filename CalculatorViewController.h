28094656e2408563fe301240373eae0e8196f337
Mike Pullen <mike.pullen@gmail.com>
2012-07-03 23:20:33 -0500

    Now under source control

---
+++ CalculatorViewController.h
@@ -0,0 +1,17 @@
+//
+//  CalculatorViewController.h
+//  Calculator
+//
+//  Created by Mike on 7/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *display;
+@property (weak, nonatomic) IBOutlet UILabel *brainHistory;
+
+
+@end
---
+++ CalculatorViewController.m
@@ -0,0 +1,107 @@
+//
+//  CalculatorViewController.m
+//  Calculator
+//
+//  Created by Mike on 7/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CalculatorViewController.h"
+#import "CalculatorBrain.h"
+
+@interface CalculatorViewController ()
+@property (nonatomic) BOOL userIsInTheMiddleOfEnteringANumber;
+@property (nonatomic, strong) CalculatorBrain *brain;
+@end
+
+@implementation CalculatorViewController
+
+@synthesize display = _display;
+@synthesize brainHistory = _brainHistory;
+@synthesize userIsInTheMiddleOfEnteringANumber = _userIsInTheMiddleOfEnteringANumber;
+@synthesize brain = _brain;
+
+- (CalculatorBrain *)brain {
+    if (!_brain) _brain = [[CalculatorBrain alloc] init];
+    return _brain;
+}
+
+- (IBAction)digitPressed:(UIButton *)sender {
+    NSString *digit = [sender currentTitle];
+    if (self.userIsInTheMiddleOfEnteringANumber) {
+        if ([digit isEqualToString:@"."]) {
+            //check to see if a decimal point is already in the display
+            NSRange range = [self.display.text rangeOfString:@"."]; 
+            if (range.location == NSNotFound) //if no decimal point then append it
+                self.display.text = [self.display.text 
+                                     stringByAppendingString:digit];
+        } else { // decimal point not pressed
+            self.display.text = [self.display.text 
+                                 stringByAppendingString:digit];
+        }
+    } else { //not in the middle of entering a number
+        self.brainHistory.text = [self.brainHistory.text 
+                                  stringByReplacingOccurrencesOfString:@"=" 
+                                  withString:@""];
+        if (![digit isEqualToString:@"0"]) {
+            if ([digit isEqualToString:@"."]) {
+                self.display.text = @"0.";
+            } else {
+                self.display.text = digit;
+            }
+            self.userIsInTheMiddleOfEnteringANumber = YES;
+        }
+    }
+}
+
+- (IBAction)enterPressed {
+    [self.brain pushOperand:[self.display.text doubleValue]];
+    self.brainHistory.text = [self.brainHistory.text 
+                              stringByAppendingFormat:@" %@",self.display.text];
+    self.userIsInTheMiddleOfEnteringANumber = NO;
+}
+
+- (IBAction)operationPressed:(UIButton *)sender {
+    NSString *operation = sender.currentTitle;
+    if ([operation isEqualToString:@"+/-"] && 
+        self.userIsInTheMiddleOfEnteringANumber) {
+        double operand = - [self.display.text doubleValue];
+        self.display.text = [NSString stringWithFormat:@"%g", operand];
+    } else {
+        if (self.userIsInTheMiddleOfEnteringANumber) {
+            [self enterPressed];
+        }
+        double result = [self.brain performOperation:operation];
+        self.display.text = [NSString stringWithFormat:@"%g", result];
+        self.brainHistory.text = [[self.brainHistory.text 
+                                  stringByReplacingOccurrencesOfString:@"=" withString:@""]
+                                  stringByAppendingFormat:@" %@ =", operation];
+    }
+    
+}
+
+- (IBAction)clearPressed {
+    [self.brain clear];
+    self.brainHistory.text = @" ";
+    self.display.text = @"0";
+    self.userIsInTheMiddleOfEnteringANumber = NO;
+}
+
+- (IBAction)backspacePressed {
+    if (self.userIsInTheMiddleOfEnteringANumber) {
+        if ([self.display.text length] > 0) {
+            self.display.text = [self.display.text substringToIndex:
+                                 [self.display.text length] - 1];
+        }
+        if ([self.display.text length] == 0) {
+            self.display.text = @"0";
+            self.userIsInTheMiddleOfEnteringANumber = NO;
+        }
+    }
+}
+
+- (void)viewDidUnload {
+    [self setBrainHistory:nil];
+    [super viewDidUnload];
+}
+@end
